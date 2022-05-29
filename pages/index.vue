b4452e1e243cecc69d80beb87528294e846ef876
Eight-Corner <corner3499@gmail.com>
2022-05-30 00:28:04 +0900

    플레이어 하단 컨트롤바 개발

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <h1 style="text-align: center;" @keydown.space="togglePlay()"> YOUTUBE - VIDEO - PLAYER </h1>
-    <div class="video-container" :class="{'paused' : paused}">
-      <div class="video-controls-container" @keydown.space="togglePlay()">
+    <h1 style="text-align: center;"> YOUTUBE - VIDEO - PLAYER </h1>
+    <div class="video-container"  data-volume-level="high"
+         :class="[{'paused' : paused}, {'theater' : theaterMode}, {'full-screen': fullscreen}, {'muted': volume === 0}]"
+         ref="video_container">
+      <div class="video-controls-container">
         <div class="timeline-container"></div>
         <div class="controls">
           <button class="play-pause-btn" @click="togglePlay()">
@@ -13,7 +15,28 @@
               <path fill="currentColor" d="M14,19H18V5H14M6,19H10V5H6V19Z" />
             </svg>
           </button>
-          <button class="theater-btn">
+
+          <div class="volume-container">
+            <button class="mute-btn" ref="muteBtn" @click="toggleMute">
+              <svg class="volume-high-icon" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z" />
+              </svg>
+              <svg class="volume-low-icon" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M5,9V15H9L14,20V4L9,9M18.5,12C18.5,10.23 17.5,8.71 16,7.97V16C17.5,15.29 18.5,13.76 18.5,12Z" />
+              </svg>
+              <svg class="volume-muted-icon" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M12,4L9.91,6.09L12,8.18M4.27,3L3,4.27L7.73,9H3V15H7L12,20V13.27L16.25,17.53C15.58,18.04 14.83,18.46 14,18.7V20.77C15.38,20.45 16.63,19.82 17.68,18.96L19.73,21L21,19.73L12,10.73M19,12C19,12.94 18.8,13.82 18.46,14.64L19.97,16.15C20.62,14.91 21,13.5 21,12C21,7.72 18,4.14 14,3.23V5.29C16.89,6.15 19,8.83 19,12M16.5,12C16.5,10.23 15.5,8.71 14,7.97V10.18L16.45,12.63C16.5,12.43 16.5,12.21 16.5,12Z" />
+              </svg>
+            </button>
+            <input class="volume-slider" ref="volumeSlider" v-model="volume" @change="volumeChange($event)"
+                   type="range" min="0" max="1" step="any" value="1" />
+          </div>
+          <button class="mini-player-btn" ref="mini" @click="toggleMiniMode">
+            <svg viewBox="0 0 24 24">
+              <path fill="currentColor" d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zm-10-7h9v6h-9z"/>
+            </svg>
+          </button>
+          <button class="theater-btn" ref="theater" @click="toggleTheater">
             <svg class="tall" viewBox="0 0 24 24">
               <path fill="currentColor" d="M19 6H5c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 10H5V8h14v8z"/>
             </svg>
@@ -21,7 +44,7 @@
               <path fill="currentColor" d="M19 7H5c-1.1 0-2 .9-2 2v6c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2zm0 8H5V9h14v6z"/>
             </svg>
           </button>
-          <button class="full-screen-btn">
+          <button class="full-screen-btn" ref="fullscreen" @click="toggleFullScreen">
             <svg class="open" viewBox="0 0 24 24">
               <path fill="currentColor" d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
             </svg>
@@ -43,16 +66,43 @@ export default {
   data() {
     return {
       paused: false,
+      theaterMode: false,
+      fullscreen : false,
+      miniMode : false,
+      volume: 1,
+      muted: false,
     }
   },
   mounted() {
+
     window.addEventListener('keyup', ((ev) => {
-      switch (ev.keyCode) {
-        case " ": break;
-        case 32: this.togglePlay(); break;
+      const key = ev.key.toLowerCase()
+      const tagName = window.document.activeElement.tagName.toLowerCase();
+      if (tagName === "input") return;
+      switch (key) {
+        case " ": if(tagName === "button") return;
+          break;
+        case "t" : this.toggleTheater(); break;
+        case "p": this.togglePlay(); break;
+        case "i": this.toggleMiniMode(); break;
+        case "f": this.toggleFullScreen(); break;
+        case "m": this.toggleMute(); break;
         default: break;
       }
+      if (ev.keyCode === 32) {
+        this.togglePlay();
+      }
     }));
+
+    document.addEventListener("fullscreenchange", () => {
+      this.$refs.video_container.classList.toggle('full-screen');
+    });
+    this.$refs.video.addEventListener('enterpictureinpicture', () => {
+      this.$refs.video_container.classList.add("mini-player");
+    });
+    this.$refs.video.addEventListener('leavepictureinpicture', () => {
+      this.$refs.video_container.classList.remove("mini-player");
+    });
   },
   methods: {
     togglePlay() {
@@ -62,6 +112,60 @@ export default {
       } else {
         this.paused = true;
         this.$refs.video.pause();
+      }
+    },
+    // volume control
+    toggleMute() {
+      const videoContainer = this.$refs.video_container;
+      const video = this.$refs.video;
+      if (!video.muted) {
+        this.volume = 0;
+        videoContainer.dataset.volumeLevel = 'muted';
+      } else {
+        this.volume = video.volume;
+        if (video.volume > .5) {
+          videoContainer.dataset.volumeLevel = 'high';
+        } else {
+          videoContainer.dataset.volumeLevel = 'low';
+        }
+      }
+      this.$refs.video.muted = !this.$refs.video.muted;
+    },
+    volumeChange(e) {
+      this.value = e.target.value;
+      this.$refs.video.volume = e.target.value;
+      this.$refs.video.muted = e.target.value === 0;
+
+      this.$refs.volumeSlider.value = this.$refs.video.volume;
+      let volumeLevel;
+      if (this.$refs.video.muted || this.$refs.video.volume === 0) {
+        this.$refs.volumeSlider.value = 0;
+        volumeLevel = "muted";
+      } else if (this.$refs.video.volume > .5){
+        volumeLevel = "high";
+      } else {
+        volumeLevel = "low";
+      }
+
+      this.$refs.video_container.dataset.volumeLevel = volumeLevel;
+
+    },
+    // view Mode
+    toggleTheater() {
+      this.$refs.video_container.classList.toggle('theater');
+    },
+    toggleFullScreen() {
+      if (document.fullscreenElement == null) {
+        this.$refs.video_container.requestFullscreen();
+      } else {
+        document.exitFullscreen();
+      }
+    },
+    toggleMiniMode() {
+      if (this.$refs.video_container.classList.contains('mini-player')) {
+        document.exitPictureInPicture();
+      } else {
+        this.$refs.video.requestPictureInPicture();
       }
     },
   },
