17d17bd57ef457b5faa75855864aa97b93135b16
cheon ki hoon <corner@sgrsoft.com>
2022-05-31 18:05:28 +0900

    video

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,10 @@ export default {
     ]
   },
 
+  server: {
+      port: 8000 // default: 3000
+  },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     'ant-design-vue/dist/antd.css',
